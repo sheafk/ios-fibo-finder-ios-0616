d102fa6e97958f7415eea5ef7e58c02059ee9aec
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-10-01 15:21:51 -0400

    cleanup- joe fix

--- test_runner.sh
+++ test_runner.sh
@@ -6,4 +6,4 @@ gunzip -c -S .xcactivitylog `ls -t | grep 'xcactivitylog' | head -n1` > output2.
 awk '{ gsub("\r", "\n"); print }' output2.log > unixfile.txt;
 LOG=`echo "puts /(^Test Suite '[\w-]+?\.xctest' started at .+?Test Suite '[\w-]+?\.xctest' (failed|passed).+?\.$)/m.match(File.read(\"unixfile.txt\"))" | ruby`;
 cd $CURR_DIR
-echo "$LOG" | xcpretty -t --report flatiron
+echo "$LOG" | xcpretty --report flatiron
